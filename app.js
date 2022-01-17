9b9c8cd3888fbac8a119d3eaf26377a8af1c0279
anthopy <87537068+anthopy@users.noreply.github.com>
2022-01-17 13:51:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ dragElement(document.getElementById("mouvement_sept"));
 dragElement(document.getElementById("mouvement_huit"));
 dragElement(document.getElementById("mouvement_neuf"));
 dragElement(document.getElementById("mouvement_dix"));
-// je ne sais pas comment plus l'optimiser
+// je ne sais pas comment plus l'optimiser,
 function up(
   zIndex1,
   zIndex2,
@@ -45,31 +45,29 @@ function dragElement(elmnt) {
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
-    // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
 
-    // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
 
   function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
-    // calculate the new cursor position:
+
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    // set the element's new position:
+
     elmnt.style.top = elmnt.offsetTop - pos2 + "px";
     elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
   }
 
   function closeDragElement() {
-    // stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
   }
 }
+// e ; étant l'élément courant, les informations disponible sur la page, sa position , le temps écoulter, les éléments cliquer etc.
